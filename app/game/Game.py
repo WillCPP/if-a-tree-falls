c98c7cc2ed70a53012f9e40eb704c71416177542
WillCPP <WillCPP@users.noreply.github.com>
2023-02-05 14:41:56 -0500

    UI updates

--- app/game/Game.py
+++ app/game/Game.py
@@ -19,6 +19,7 @@ class Game:
         pygame.init()
         self.ui = UI()
         self.fg.load_images()
+        self.fg.addFGElement([{"file": "Root_N", "x": 15, "y": 5}])
         pygame.key.set_repeat(250)
         screen = pygame.display.set_mode((1024, 480))
         clock = pygame.time.Clock()
@@ -40,10 +41,11 @@ class Game:
                     if keys[pygame.K_RETURN] or keys[pygame.K_SPACE]:
                         self.fg.addFGElement(self.tree.tryToAddRoot(int(self.cursor.pos_x / 32), int(self.cursor.pos_y / 32)))
             
+            self.tree.update()
             self.bg.draw_background(screen)
             self.fg.draw_foreground(screen)
             self.cursor.draw(screen)
-            self.ui.update_ui_surface(self.cursor, self.tree.resourceStock)
+            self.ui.update_ui_surface(self.tree.BlockGrid[int(self.cursor.pos_y / 32)][int(self.cursor.pos_x / 32)]["BlockId"], self.tree.resourceStock, self.tree.treeHealth.currentHealth)
             self.ui.draw_ui(screen)
             pygame.display.update()
 
